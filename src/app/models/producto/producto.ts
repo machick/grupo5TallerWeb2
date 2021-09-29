b6e15166259a0ce22b5161f38f764640b4b2a042
MatiasGuerrero61 <guerreromatias24@gmail.com>
2021-09-28 23:24:54 -0300

    clase usuario y producto

---
+++ src/app/models/producto/producto.ts
@@ -0,0 +1,8 @@
+export class Producto {
+    constructor(
+        public nombre:string,
+        public descripcion:string,
+        public clasificacion:string,
+        public precio:number
+    ){}
+}
