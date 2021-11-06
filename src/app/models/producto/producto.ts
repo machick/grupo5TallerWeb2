088b80b05522a50a91ef4b87c95117c85131e7ea
Jose Machicado <josemiguelmc_1998@hotmail.com>
2021-11-06 01:39:58 -0300

    products

--- src/app/models/producto/producto.ts
+++ src/app/models/producto/producto.ts
@@ -10,7 +10,6 @@ export class Producto {
     /*se crea un metodo para serializar el json que recibe desde la api
     este sera devuelto como un objeto tengo entendido*/
     public static serializarProducto(json:any):Producto{
-        
-        return new this(json.codigo_producto, json.nombre, json.descripcion, json.clasificacion, json.precio);
+        return new this(json._id, json.name, json.description, json.type, json.price);
       }
 }
