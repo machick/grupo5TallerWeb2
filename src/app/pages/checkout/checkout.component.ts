82143aaaed36e881a18555acdcfdd5be20e11117
MatiasGuerrero61 <guerreromatias24@gmail.com>
2021-11-25 01:27:15 -0300

    vista checkout con datos de la base de datos

--- src/app/pages/checkout/checkout.component.ts
+++ src/app/pages/checkout/checkout.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Carrito } from 'src/app/models/carrito/carrito';
+import { CarritoService } from 'src/app/services/carrito.service';
+
 
 @Component({
   selector: 'app-checkout',
@@ -6,10 +9,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./checkout.component.css']
 })
 export class CheckoutComponent implements OnInit {
+  public carrito : any;
+  public subtotal = 0;
+  public cantidad = 0;
 
-  constructor() { }
+  
+
+  constructor(
+    private carritoService : CarritoService) 
+    { }
 
   ngOnInit(): void {
+    this.obtenerCarrito();
   }
 
+  public obtenerCarrito(){
+
+    return this.carritoService.obtenerCarrito()
+        .then((carritoObtenido) => {
+          this.carrito= carritoObtenido;
+          this.calcularPrecio();
+        })
+        .catch(() => {
+          console.log('error obtener carrito');
+        });
+  }
+
+  public calcularPrecio(){
+    this.carrito.items.forEach((item: any) => {
+      this.cantidad += item.cantidad;
+      this.subtotal += item.cantidad * item.producto.precio;
+    });
+  }
 }
