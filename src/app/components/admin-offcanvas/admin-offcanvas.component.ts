a32aafae44ed8cc7d38fb88bb7ee439825e6d8c3
ignaciodamiang <ignacigonzalez@alumno.unlam.edu.ar>
2021-11-25 00:21:30 -0300

    Agregar producto

--- src/app/components/admin-offcanvas/admin-offcanvas.component.ts
+++ src/app/components/admin-offcanvas/admin-offcanvas.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductoserviceService } from 'src/app/services/productoservice.service';
 
 @Component({
   selector: 'app-admin-offcanvas',
   templateUrl: './admin-offcanvas.component.html',
-  styleUrls: ['./admin-offcanvas.component.css']
+  styleUrls: ['./admin-offcanvas.component.css'],
 })
 export class AdminOffcanvasComponent implements OnInit {
+  public newNombre: any;
+  public newDescripcion: any;
+  public newTipo: any;
+  public newPrecio: any;
+  constructor(private productoService: ProductoserviceService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  public agregarProducto() {
+    return this.productoService
+      .agregarProducto(
+        this.newNombre,
+        this.newDescripcion,
+        this.newTipo,
+        this.newPrecio
+      )
+      .then(() => {
+        window.location.reload();
+      });
   }
-
 }
