916b70d47d7be8a5e2c8d0de77b3d4cdc5d09412
Jose Machicado <josemiguelmc_1998@hotmail.com>
2021-11-28 21:45:09 -0300

    fix whitelist ng serve ng serve

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  API:'http://localhost:3000',
-  API_WHITELIST:['localhost:3000'],
-  API_URL_IMAGE: 'http://localhost:3000',
+  API:'https://birraapp-api-2pile.ondigitalocean.app',
+  API_WHITELIST:['birraapp-api-2pile.ondigitalocean.app'],
+  API_URL_IMAGE: 'https://birraapp-api-2pile.ondigitalocean.app',
   FILE_SIZE: 5000000,
   FILE_SIZE_TEXT: 5,
 };
